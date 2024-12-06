070db1c3213e249dfe77a7d84f1ed30d93829953
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-05 19:30:14 -0500

    Test: Created test function to return data from a file specified by a file path

--- tests/fileOperations.test.js
+++ tests/fileOperations.test.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { writeFile } = require('../fileOperations');
+const { writeFile, readFile } = require('../fileOperations');
 
 describe('File Operations', () => {
     const testFilePath = path.join(__dirname, 'testFileWrite.txt');
@@ -17,5 +17,13 @@ describe('File Operations', () => {
         const writtenContent = fs.readFileSync(testFilePath, 'utf8');
         expect(writtenContent).toBe(content);
     });//end of test 
+
+    test('should read content from a file', () => {
+        const expectedContent = 'Hello, World!';
+        fs.writeFileSync(testFilePath, expectedContent);
+        const readContent = readFile(testFilePath);
+        expect(readContent).toBe(expectedContent);
+    });//end of test
+
 
 });//end of describe
