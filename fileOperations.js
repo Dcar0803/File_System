848458e5d0af07a136e6876328a9515c9f1165e4
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-05 19:15:30 -0500

    Doc: Created documentation for the writeFile() function

--- fileOperations.js
+++ fileOperations.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 
+/**
+ * Writes content to a specified file path.
+ * @param {string} filePath - The path to the file.
+ * @param {string} content -The content to write.
+
+ */
+
 function writeFile(filePath, content){
     fs.writeFileSync(filePath, content, 'utf8');
 
